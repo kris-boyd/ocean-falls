3a0c5d76b259a4c5179920bc39e823f8b2d9524c
Kristopher boyd <krisboyd1@gmail.com>
2022-11-12 15:35:50 -0800

    remove useState and Button to get rid of warnings that prevent Netlify build

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect } from 'react';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 import WelcomeModal from './WelcomeModal.js'
 import ImageModal from './ImageModal.js'
