1d8f934a6104718197f5a77b63120e91e976404d
Sunki Baek <sunki.baek@gmail.com>
2023-04-16 11:12:30 -0700

    Convert js to ts

--- src/StaticMap.tsx
+++ src/StaticMap.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 
-export default function StaticMap({ width, height }) {
+export default function StaticMap({ width, height }: IDimension) {
   return (
     <div
-      width={width}
-      height={height}
       style={{
         backgroundImage: `url('https://api.mapbox.com/styles/v1/krisboyd/cl6leg7ea000w14mrkl03b1yn/static/-127.6906,52.3518,16/${width}x${height}?access_token=pk.eyJ1Ijoia3Jpc2JveWQiLCJhIjoiY2w2azVpcXdxMTlyMDNjbzJ5dWIxODZxaSJ9.-WDtw9QaqwiPtZyokBre6Q')`,
         backgroundRepeat: "no-repeat",
@@ -14,6 +12,6 @@ export default function StaticMap({ width, height }) {
         position: "absolute",
         top: 0,
       }}
-    ></div>
+    />
   );
 }
